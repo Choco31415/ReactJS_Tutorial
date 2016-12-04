7e5d55f68b9d36f7c25a4ca010498429d2f7d86e
Choco31415 <Eparke31415@gmail.com>
2016-12-04 18:00:22 -0500

    Create index.js

---
+++ Step2/index.js
@@ -0,0 +1,9 @@
+/*
+A simple game showing off component functions and inserting JS into HTML tags.
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
